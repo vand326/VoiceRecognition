7110de5469f10096a6c8a05d6a7e4b7b33170e65
vand326 <42849525+vand326@users.noreply.github.com>
2019-10-28 17:59:34 -0700

    sketch

--- sketch.js
+++ sketch.js
@@ -1,18 +1,31 @@
-var mic;
+let mic, fft, w;
 
 function setup() {
-  createCanvas(700,700);
+ createCanvas(700,700);
+ colorMode(HSB);
  mic = new p5.AudioIn();
  mic.start();
+ fft = new p5.FFT(0.9, 512);
+ fft.setInput(mic);
+ w = width/100;
 }
 
 function draw() {
  background(0);
- var vol = mic.getLevel();
 
+ var spectrum = fft.analyze();
+ console.log(spectrum);
+  noStroke();
+  // translate(width/2, height/2);
+  for (var i = 0; i < spectrum.length; i++) {
+   var amp = spectrum[i];
+   var y = map(amp, 0, 100, height, 0);
+   fill(i, 255, 255);
+   rect(i * w, y, w- 2, height - y);
+  }
+  stroke(255);
+  noFill();
 
- ellipse(350, 350, vol * 1000 , vol * 1000);
- console.log(vol);
 
 
 }
