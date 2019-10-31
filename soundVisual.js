e84c9a9240d970490c44c966cafb28b3753549b5
vand326 <42849525+vand326@users.noreply.github.com>
2019-10-30 19:39:39 -0700

    script combined

--- soundVisual.js
+++ soundVisual.js
@@ -1,9 +1,15 @@
 let mic, fft, w;
 var canvas;
 
+function windowResized() {
+ resizeCanvas(windowWidth, windowHeight);
+}
+
+
 function setup() {
- canvas = createCanvas(1400,700);
- canvas.position(300,7300);
+ canvas = createCanvas(windowWidth, windowHeight);
+ canvas.position(0,0);
+ canvas.style('z-index', '-1');
 
  colorMode(HSB);
  mic = new p5.AudioIn();
@@ -11,13 +17,26 @@ function setup() {
  fft = new p5.FFT(0.9, 512);
  fft.setInput(mic);
  w = width/100;
+
+ let lang = navigator.language || 'en-US';
+ let speechRec = new p5.SpeechRec(lang, gotSpeech);
+
+ let continuous = true;
+ let interm = false;
+ speechRec.start(continuous, interm);
+
+ function gotSpeech() {
+   if (speechRec.resultValue) {
+     createP(speechRec.resultString);
+   }
+ }
 }
 
 function draw() {
  background(255);
 
  var spectrum = fft.analyze();
- console.log(spectrum);
+ // console.log(spectrum);
   noStroke();
   // translate(width/2, height/2);
   for (var i = 0; i < spectrum.length; i++) {
